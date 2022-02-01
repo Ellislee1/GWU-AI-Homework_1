07ce80eeedf61fb275c3b36da017a401aa634609
Ellis Thompson <>
2022-02-01 12:29:17 +0000

    Added method to get the values of each action

--- Environment.py
+++ Environment.py
@@ -1,3 +1,4 @@
+from readline import get_history_item
 from this import d
 import numpy as np
 
@@ -8,6 +9,14 @@ class Environment:
         self.steps = 0
 
         self.volumes = np.zeros(len(pitchers)+1)
+
+        self.actions = {}
+
+        self.actions["Fill"] = np.zeros(len(pitchers))
+        self.actions["Empty"] = np.zeros(len(pitchers)+1)
+        self.actions["Transfer"] = np.zeros((len(pitchers)+1,len(pitchers)+1))
+
+        self.get_h()
     
     def fill (self, source, dest):
         if source == -2:
@@ -35,12 +44,65 @@ class Environment:
                     self.volumes[source] -= to_move
 
                     self.steps += 1
+        self.get_h()
 
     def get_state(self):
         return self.volumes
 
+    def get_h(self):
+        cur_dist = self.get_distance()
+
+        # Get the distances for filling a pitcher
+        for i in range(len(self.actions["Fill"])):
+            if self.volumes[i] == self.pitchers[i]:
+                self.actions["Fill"][i] = 0
+                continue
+            diff = self.pitchers[i] - self.volumes[i]
+            self.actions["Fill"][i] = cur_dist+diff
+        
+        # Get the distance for emptying a pitcher
+        for i in range(-1,len(self.pitchers)):
+            if self.volumes[i] == 0:
+                self.actions["Empty"][i] = 0
+                continue
+
+            diff = self.pitchers[i]
+            self.actions["Empty"][i] = cur_dist-diff
+
+        # Get the distance for transfering liquid
+        for i in range(-1,len(self.pitchers)):
+            # Cannot transfer if there is nothing in pitcher[i], so continue
+            if self.volumes[i] == 0:
+                for j in range(-1,len(self.pitchers)):
+                    self.actions["Transfer"][i,j] = 0
+                continue
+
+            for j in range(-1,len(self.pitchers)):
+                # Can not transfer volume to itsself
+                if i == j:
+                    continue
+                
+                # Cannot transfer to an already full container
+                if j >= 0 and self.volumes[j] == self.pitchers[j]:
+                    self.actions["Transfer"][i,j] = 0
+                    continue
+
+                if j == -1:
+                    diff = cur_dist + self.volumes[-1]+self.volumes[i]
+                    continue
+
+                dest_diff = self.pitchers[j] - self.volumes[j]
+                to_transfer = min(self.volumes[i],dest_diff)
+
+                best= abs(min((self.volumes[i]-to_transfer)-self.goal,(self.volumes[j]-to_transfer)-self.goal))
+                self.actions["Transfer"][i,j] = best
+
+
     def get_distance(self):
         return np.abs(self.goal - np.sum(self.volumes))
+    
+    def get_experimental_dist(self, var=None):
+        pass
     
     def __str__(self) -> str:
         headers = ""
@@ -56,4 +118,4 @@ class Environment:
             volumes += f'{i}\t'
 
         return f'\n---------------------------------\n------ Current Environment ------\n---------------------------------'\
-        f'\n{headers}\n{volumes}\n\nDistance: {self.get_distance()}\n\n\t\t Total Steps: {self.steps}'
+        f'\n{headers}\n{volumes}\n\nDistance: {self.get_distance()}\n{self.actions}\n\n\t\t Total Steps: {self.steps}'
