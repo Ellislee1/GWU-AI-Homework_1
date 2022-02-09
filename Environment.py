ac89107755f9e0212174b57b4b5937d501a1ee4c
Ellis Thompson <EllisThompson.1999@outlook.com>
2022-02-09 10:31:01 -0500

    Better formatting in Environment

--- Environment.py
+++ Environment.py
@@ -27,24 +27,39 @@ class Environment:
         
         new_states = []
 
-        # All Filling states, this ignores filling the infinite pitcher
+        new_states = self.get_fill_max(new_states)
+        new_states = self.get_empty(new_states)
+        new_states = self.get_transfer(new_states)
+
+        return new_states
+    
+    def get_fill_max(self, states):
+        """All Filling states, this ignores filling the infinite pitcher"""
         for i in range(len(self.pitchers)):
             if self.volumes[i] == self.pitchers[i]:
                 continue
 
             copy = np.copy(self.volumes)
             copy[i] = self.pitchers[i]
-            new_states.append(np.append(copy, self.steps + 1))
+            states.append(np.append(copy, self.steps + 1))
+        
+        return states
 
-        # Add Emptying States, Empty all pitchers to 0 (if pitcher have some volume)
+    def get_empty(self, states):
+        """Add Emptying States, Empty all pitchers to 0 
+        (if pitcher have some volume)"""
         for i in range(len(self.volumes)):
             if self.volumes[i] == 0:
                 continue
             copy = np.copy(self.volumes)
             copy[i] = 0
-            new_states.append(np.append(copy, self.steps + 1))
+            states.append(np.append(copy, self.steps + 1))
+        
+        return states
 
-        # Add Transfer States
+    def get_transfer(self, states):
+        """Add Transfer States by looping through all possible source
+        states and destination states and making valid transfers"""
         for i in range(-1, len(self.volumes) - 1):
             if self.volumes[i] == 0:
                 continue
@@ -53,21 +68,17 @@ class Environment:
                     continue
                 
                 # Dump all into infinate state "-1"
+                copy = np.copy(self.volumes)
                 if j == -1:
-                    copy = np.copy(self.volumes)
-                    copy[j] += copy[i]
-                    copy[i] = 0
-                    new_states.append(np.append(copy, self.steps + 1))
+                    transfer = copy[i]
                 else:
-                    copy = np.copy(self.volumes)
                     dest_diff = self.pitchers[j] - copy[j]
-                    worst = min(dest_diff, copy[i])
-                    copy[j] += worst
-                    copy[i] -= worst
+                    transfer = min(dest_diff, copy[i])
 
-                    new_states.append(np.append(copy, self.steps + 1))
-
-        return new_states
+                copy[j] += transfer
+                copy[i] -= transfer 
+                states.append(np.append(copy, self.steps + 1))
+        return states
 
     def load_env_state(self, volumes, steps):
         self.volumes = volumes
